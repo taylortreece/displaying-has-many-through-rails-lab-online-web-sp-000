b535195d7f5eef2769699e04e590b872e7df355f
taylortreece <tayloratreece@gmail.com>
2021-04-24 01:42:57 -0500

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,9 +1,5 @@
 class AppointmentsController < ApplicationController
 
-    def index
-        @appointments=Appointment.all
-    end
-
     def show
         @appointment=Appointment.find(params[:id])
     end
